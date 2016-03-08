3c6d7017667c89141ec8012994ee54b7e3525ff9
Jim <icsd11053@icsd.aegean.gr>
2016-03-08 23:29:32 +0200

    Complete the ChatRoom interface Fix Splash Add SnackBar on Connection activity

--- app/src/main/java/jk/dev/cryptomessaging/Connection.java
+++ app/src/main/java/jk/dev/cryptomessaging/Connection.java
@@ -1,13 +1,37 @@
 package jk.dev.cryptomessaging;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Connection extends AppCompatActivity {
 
+    int backPressedCounter = 0;
+    private CoordinatorLayout coordinatorLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id
+                .coordinatorLayout);
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+        if (backPressedCounter == 0) {
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, "Press back again to exit", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }else {
+            super.onBackPressed();
+        }
+        backPressedCounter++;
     }
 }
--- app/src/main/java/jk/dev/cryptomessaging/Splash.java
+++ app/src/main/java/jk/dev/cryptomessaging/Splash.java
@@ -1,7 +1,11 @@
 package jk.dev.cryptomessaging;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                startActivity(new Intent(Splash.this, Connection.class));
+            }
+        }, 2500);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+
     }
 }
